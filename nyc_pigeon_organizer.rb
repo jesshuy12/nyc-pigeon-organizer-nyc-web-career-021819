80bc362b9995f1c24d180ee4fa9b339f37c87b27
Jessy Hu <jesshuy12@gmail.com>
2019-02-18 02:45:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,5 @@
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   pigeon_list = {}                            #creating new hash to store in
     data.each do |key, value|                 #iterate over data
       value.each do |attribute, names|        #iterate over value
@@ -11,15 +12,29 @@ def nyc_pigeon_organizer(data)
            pigeon_list[name][key] = []                    #create new array
          end
          if !pigeon_list[name][key].include?(attribute)   #returns true if (attribute) is present
-           pigeon_list[name][key] << attribute.to_s
+           pigeon_list[name][key] << attribute.to_s       #push attribute convt to string to key  
            
          end
        end
      end
    end
-   pigeon_list
+   return pigeon_list   
  end
           
           
           
     
+=======
+  pigeon_list = {}
+    data.each do |key, value|
+      value.each do |attribute, name|
+        name.each do |name|
+          
+          if pigeon_list[name][key] ||= {}
+          pigeon_list[name][key] << attribute.to_s
+        end
+      end
+    end
+    return pigeon_list
+end
+>>>>>>> b5c09b3d020ab959a250c422f54c51ee50b38a5e
